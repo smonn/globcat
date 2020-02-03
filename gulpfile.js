b4a324acc7421893be9740dc2a686fc9c755337d
Simon Ingeson <44818+smonn@users.noreply.github.com>
2020-02-03 13:14:37 -0500

    updating dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,18 @@
 const gulp = require('gulp');
 const jscs = require('gulp-jscs');
 const jshint = require('gulp-jshint');
+const mocha = require('gulp-mocha');
 
 const src = ['globcat.js', 'test/**/*.js', 'bin/**/*.js'];
 
-gulp.task('style', function() {
+gulp.task('default', function() {
   return gulp.src(src)
     .pipe(jscs())
-    .pipe(jscs.reporter());
-});
-
-gulp.task('lint', function() {
-  return gulp.src(src)
+    .pipe(jscs.reporter())
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter('jshint-stylish'))
-    .pipe(jshint.reporter('fail'));
+    .pipe(jshint.reporter('fail'))
+    .pipe(mocha())
+    .once('error', () => process.exit(1))
+    .once('end', () => process.exit());
 });
-
-gulp.task('default', ['style', 'lint']);
