dc28923061c81f3ccabac86b647580f117bab8cf
Simon Ingeson <44818+smonn@users.noreply.github.com>
2023-12-14 22:04:26 -0500

    fix some async tests

--- src/globcat.spec.ts
+++ src/globcat.spec.ts
@@ -24,7 +24,7 @@ test('should return empty string if nothing matches', async () => {
 
 test('should stream file content', async () => {
   const cwd = process.cwd()
-  const pattern = join(cwd, 'test-files/small**/*.txt')
+  const pattern = join(cwd, 'test-files/small/**/*.txt')
 
   const stream = await globcat(pattern, { stream: true })
   assert.equal(isReadableStream(stream), true, 'should be a readable stream')
@@ -39,38 +39,32 @@ test('should fail when matching directory', async () => {
   ).rejects.toThrowError(/^Not a file:/)
 })
 
-test('should support callback function', () => {
+test('should support callback function', async () => {
   const cwd = process.cwd()
-  const pattern = join(cwd, 'test-files/small/**/*.txt')
+  const pattern = join(cwd, 'test-files/small/*.txt')
 
-  return new Promise<void>((done) => {
+  const content = await new Promise<string>((resolve, reject) => {
     globcat(pattern, { stream: false }, (err, content) => {
-      assert.notExists(err, 'no errors')
-      assert.match(
-        content ?? 'no content',
-        /bar\s+baz\s+foo\s+/,
-        'should equal file contents'
-      )
-      done()
+      if (err) reject(err)
+      else resolve(content ?? '')
     })
   })
+
+  assert.match(content, /bar\nbaz\nfoo\n/, 'should equal file contents')
 })
 
-test('should allow second argument to be callback', () => {
+test('should allow second argument to be callback', async () => {
   const cwd = process.cwd()
-  const pattern = join(cwd, 'test-files/small/**/*.txt')
+  const pattern = join(cwd, 'test-files/small/*.txt')
 
-  return new Promise<void>((done) => {
+  const content = await new Promise<string>((resolve, reject) => {
     globcat(pattern, (err, content) => {
-      assert.notExists(err, 'no errors')
-      assert.match(
-        content ?? 'no content',
-        /bar\s+baz\s+foo\s+/,
-        'should equal file contents'
-      )
-      done()
+      if (err) reject(err)
+      else resolve(content ?? '')
     })
   })
+
+  assert.match(content, /bar\nbaz\nfoo\n/, 'should equal file contents')
 })
 
 test('should yield error when using callback', () => {
@@ -88,19 +82,13 @@ test('should yield error when using callback', () => {
   })
 })
 
-test('should correctly concat large files', () => {
+test('should correctly concat large files', async () => {
   const cwd = process.cwd()
   const pattern = join(cwd, 'test-files/large/**/*.txt')
-
-  return new Promise<void>((done) => {
-    globcat(pattern, (err, content) => {
-      assert.notExists(err, 'no errors')
-      assert.match(
-        content ?? 'no content',
-        /[a\n]{10000}[b\n]{10000}/,
-        'should equal file contents'
-      )
-      done()
-    })
-  })
+  const content = await globcat(pattern)
+  assert.match(
+    content,
+    /[a\n]{10000}[b\n]{10000}/,
+    'should equal file contents'
+  )
 })
